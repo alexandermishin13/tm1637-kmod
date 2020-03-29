b44297d5d874ac1a5957b6ffb9a2db1e5cc2be8c
Александр Мишин <mishin@gecko.mh.net.ru>
2020-03-30 01:00:24 +0400

    Added a headers file

--- tm1637_kmod.c
+++ tm1637_kmod.c
@@ -1,6 +1,18 @@
-/*
- * KLD tm1637 display
- * Mishin Alexander, 2 Feb 2020
+/*-
+ * Copyright (C) 2020 Alexander Mishin.
+ * All rights reserved.
+ *
+ * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
 #define FDT
@@ -21,38 +33,13 @@
 
 #include <dev/gpio/gpiobusvar.h>
 
-#define	TM1637_CDEV_NAME	"tm1637"
-#define	TM1637_SCL_PROPERTY	"scl-gpios"
-#define	TM1637_SDA_PROPERTY	"sda-gpios"
-#define	TM1637_SCL_IDX		0
-#define	TM1637_SDA_IDX		1
-#define	TM1637_MIN_PINS		2
+#include "tm1637_kmod.h"
 
-#define	TM1637_ACK_TIMEOUT	200
-
-#define	TM1637_ADDRESS_AUTO	0x40
-#define	TM1637_ADDRESS_FIXED	0x44
-#define TM1637_START_ADDRESS	0xc0
-
-#define	TM1637_BRIGHT_DARKEST	0
-#define	TM1637_BRIGHT_TYPICAL	2
-#define	TM1637_BRIGHTEST	7
-
-#define	TM1637_MAX_COLOM	4
 #define	TM1637_COLON_POSITION	2 // 2-st from zero character if not a digit or a sign
 #define	TM1637_BUFFERSIZE	TM1637_MAX_COLOM + 3 // For ':', '\n' and '\0'
 
 #define SIGN_MINUS		0x40
 #define SIGN_EMPTY		0x00
-
-#define	TM1637_LOCK_INIT(sc)						\
-    mtx_init(&(sc)->lock, "tm1637 mtx", NULL, MTX_DEF)
-#define	TM1637_LOCK_DESTROY(sc)						\
-    mtx_destroy(&(sc)->lock)
-#define	TM1637_LOCK(sc)							\
-    mtx_lock(&(sc)->lock)
-#define	TM1637_UNLOCK(sc)						\
-    mtx_unlock(&(sc)->lock)
 
 static u_char char_code[] = { 0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f };
 
@@ -66,22 +53,6 @@ static int tm1637_detach(device_t);
 struct s_message {
     char text[TM1637_BUFFERSIZE + 1]; // ??? +1
     int len;
-};
-
-struct tm1637_softc {
-    device_t		 tm1637_dev;
-    gpio_pin_t		 tm1637_sclpin;
-    gpio_pin_t		 tm1637_sdapin;
-    uint8_t		 tm1637_brightness;
-    uint8_t		 tm1637_on;
-    uint8_t		 tm1637_raw_format;
-    bool		 tm1637_inuse;
-    bool		 tm1637_colon;
-    u_char		 tm1637_digits[TM1637_MAX_COLOM + 1]; // ??? 1 byte overflow
-    u_char		 tm1637_digits_prev[TM1637_MAX_COLOM];
-    struct mtx		 lock;
-    struct cdev		*tm1637_cdev;
-    struct s_message	*tm1637_msg;
 };
 
 static void tm1637_display_on(struct tm1637_softc *sc);
@@ -513,21 +484,13 @@ tm1637_msg_decode(struct tm1637_softc *sc)
 #endif
 
 	if(c>='0' && c<='9') // encode if digit
-	{
 	    sc->tm1637_digits[position++] = char_code[c&0x0f];
-	}
 	else if(c=='-') // '-' sign
-	{
 	    sc->tm1637_digits[position++] = SIGN_MINUS;
-	}
 	else if(c=='#') // No changes if wildcard
-	{
 	    position++;
-	}
 	else if(c=='\n') // End of chunk
-	{
 	    break;
-	}
 	else if(buf_index==2)
 	{
 	    // Set a clockpoint or return an error
@@ -579,7 +542,7 @@ tm1637_msg_decode(struct tm1637_softc *sc)
 	uprintf("type: number\n");
 #endif
 
-	// Add trailing spaces
+	// Add trailing spaces starting from last position
 	for (;position <= TM1637_MAX_COLOM; position++)
 	    sc->tm1637_digits[position] = 0x00;
 
@@ -905,4 +868,3 @@ tm1637_attach(device_t dev)
 
     return (bus_generic_attach(dev));
 }
-
---
+++ tm1637_kmod.h
@@ -0,0 +1,65 @@
+/*-
+ * Copyright (C) 2020 Alexander Mishin.
+ * All rights reserved.
+ *
+ * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _TM1637_KMOD_H_
+#define _TM1637_KMOD_H_
+
+#define	TM1637_CDEV_NAME	"tm1637"
+#define	TM1637_SCL_PROPERTY	"scl-gpios"
+#define	TM1637_SDA_PROPERTY	"sda-gpios"
+#define	TM1637_SCL_IDX		0
+#define	TM1637_SDA_IDX		1
+#define	TM1637_MIN_PINS		2
+
+#define	TM1637_ACK_TIMEOUT	200
+
+#define	TM1637_ADDRESS_AUTO	0x40
+#define	TM1637_ADDRESS_FIXED	0x44
+#define TM1637_START_ADDRESS	0xc0
+
+#define	TM1637_BRIGHT_DARKEST	0
+#define	TM1637_BRIGHT_TYPICAL	2
+#define	TM1637_BRIGHTEST	7
+
+#define	TM1637_MAX_COLOM	4
+
+struct tm1637_softc {
+    device_t		 tm1637_dev;
+    gpio_pin_t		 tm1637_sclpin;
+    gpio_pin_t		 tm1637_sdapin;
+    uint8_t		 tm1637_brightness;
+    uint8_t		 tm1637_on;
+    uint8_t		 tm1637_raw_format;
+    bool		 tm1637_inuse;
+    bool		 tm1637_colon;
+    u_char		 tm1637_digits[TM1637_MAX_COLOM + 1]; // ??? 1 byte overflow
+    u_char		 tm1637_digits_prev[TM1637_MAX_COLOM];
+    struct mtx		 lock;
+    struct cdev		*tm1637_cdev;
+    struct s_message	*tm1637_msg;
+};
+
+#define	TM1637_LOCK_INIT(sc)						\
+    mtx_init(&(sc)->lock, "tm1637 mtx", NULL, MTX_DEF)
+#define	TM1637_LOCK_DESTROY(sc)						\
+    mtx_destroy(&(sc)->lock)
+#define	TM1637_LOCK(sc)							\
+    mtx_lock(&(sc)->lock)
+#define	TM1637_UNLOCK(sc)						\
+    mtx_unlock(&(sc)->lock)
+
+#endif /* _TM1637_KMOD_H_ */
